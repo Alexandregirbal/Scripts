2912bdb6e7af1cf7a95642230253e74de2696aa5
Alexandre <alexandre.girbal.pro@gmail.com>
2021-03-04 00:08:07 +0100

    v0.1

---
+++ PDFAnnotator/config.template.py
@@ -0,0 +1,13 @@
+#1: change the name of this file to "config.py"
+#2: change every field of this config file as you wish
+
+firstName = "John" #case IS NOT important
+lastName = "John" #case IS NOT important
+genres = ["Monsieur", "Madame"] #do not change
+name = genres[-1] + ' ' + lastName.upper() + ' ' + firstName.upper() #replace -1 by 0 (Monsieur) or 1 (Madame)
+adress = "1 rue de la Tulipe" #case IS important
+city = "75000 Paris" #case IS NOT important
+birthDate = "01/01/1950" #format: DD/MM/YYYY
+date = "31/12/20" #format: DD/MM/YY
+hour = "09H12" #format: hh mm (do not forget the H)
+beautifulDate = "lundi 31 décembre 2020" #Must be the same date as the "date" parameter - format: dayOfWeek DD month YYYY - case IS important
